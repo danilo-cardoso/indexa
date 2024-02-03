0bdae77388a6236a2bde3a5b96d2f2f701c8cb84
Nialods <105827271+Nialods@users.noreply.github.com>
2024-02-02 22:16:37 -0300

    criando componente separador de contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SeparatorComponent } from './components/separator/separator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
