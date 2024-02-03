8d4c04ca930ef55b7e0c3f86c833d32225919f08
Nialods <105827271+Nialods@users.noreply.github.com>
2024-02-02 22:13:39 -0300

    criando variaveis de estilo e importando imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'indexa';
